e4c50a9dab4c700276d03e3efa9cc95d6f31c05b
Cody <Cojo341@gmail.com>
2022-09-08 10:21:45 -0400

    Finished TODO's

---
+++ BestBuyBestPractices/IDepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestBuyBestPractices
+{
+    internal interface IDepartmentRepository
+    {
+        public IEnumerable<Departement> GetAllDepartments();
+
+        public void InsertDepartement(string newDepartment);
+    }
+}
